4815b3e4a52b8829375d21222fd0d5a91ccb9e33
d.korchagin <d.korchagin@corp.mail.ru>
2020-02-13 02:31:50 +0300

    done with sqlite, try to decode values (step1: generate cipher key)

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,52 @@
 extern crate os_type;
 extern crate dirs;
+extern crate sqlite;
+extern crate crypto;
 
-use std::process;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
+#[derive(Debug)]
+struct Cookie {
+    host: String,
+    name: String,
+    value: String,
+    encrypted_value: String,
+}
 
-fn get_cookie_default_path() -> Option<String> {
+struct OSSpecificParams {
+    cookie_path: Option<PathBuf>,
+    iterations: usize,
+}
 
-    let home_dir = match dirs::home_dir() {
-        Some(path) => match path.to_str() {
-            Some(p) => p.to_owned(),
-            None => return None,
+fn get_os_params() -> OSSpecificParams {
+
+    let mut s_params = OSSpecificParams {
+        cookie_path: dirs::home_dir(),
+        iterations: 0
+    };
+
+    match os_type::current_platform().os_type {
+        os_type::OSType::OSX => {
+            s_params.cookie_path.as_mut().unwrap().push(Path::new("Library/Aplication Support/Google/Chrome/Default/Cookies"));
+            s_params.iterations = 32;
         },
-        None => return None,
-    };
-    let os_rel_path = match os_type::current_platform().os_type {
-        os_type::OSType::OSX => home_dir.to_owned() + "/Library/Application Support/Google/Chrome/Default/Cookies",
         os_type::OSType::Unknown => {
-            return None
-        }
-        _ => "linux default path".to_string(),
+            s_params.cookie_path = None
+
+        },
+        _ => {
+            s_params.cookie_path.as_mut().unwrap().push(Path::new("SomeLinuxPath"));
+            s_params.iterations = 1;
+        },
     };
-    return Some(os_rel_path);
+    return s_params;
 }
 
 
-fn read_user_input(input_msg: String) -> String {
+fn read_user_input(a_input_msg: &str) -> String {
     use std::io::{stdin,stdout,Write};
     let mut raw=String::new();
-    print!("{}", input_msg);
+    print!("{}", a_input_msg);
     let _=stdout().flush();
     stdin().read_line(&mut raw).expect("Did not enter a correct string");
     if let Some('\n')=raw.chars().next_back() {
@@ -40,20 +58,55 @@ fn read_user_input(input_msg: String) -> String {
     return raw;
 }
 
+fn extract_cookies(a_path: &std::path::Path) -> Vec<Cookie> {
+    let connection = sqlite::open(a_path).unwrap();
+
+    let mut statement = connection
+        .prepare("SELECT host_key, name, encrypted_value FROM cookies")
+        .unwrap();
+
+    let mut res: Vec<Cookie> = Vec::new();
+
+    while let sqlite::State::Row = statement.next().unwrap() {
+        res.push(Cookie{
+            host: statement.read::<String>(0).unwrap(),
+            name: statement.read::<String>(1).unwrap(),
+            value: String::new(),
+            encrypted_value: statement.read::<String>(2).unwrap(),
+        });
+    };
+    return res;
+}
+
+fn get_cipher_key(a_password: &mut str, a_iterations: usize) -> String {
+    let mut res = [0u8; 16];
+    return String::new();
+//    crypto::pbkdf2::pbkdf2(&mut a_password.as_bytes(), "saltysalt".as_bytes(), a_iterations as u32, &mut res);
+//    return unsafe { String::from_raw_parts(res.as_mut_ptr(), 16, 16) };
+}
+
+fn decrypt_cookie(a_cookie: &mut Cookie) {
+
+}
+
 fn main() {
     //let user_password = read_pass();
     //println!("Entered password: {}", user_password);
 
-    let cookie_file = match get_cookie_default_path() {
-        Some(p) => Path::new(&p.to_owned()),
-        None => Path::new(&String::from_string(read_user_input("Fail to get cookie path automatically. Please input it manually: ".to_string()))),
-    };
+    let mut os_params = get_os_params();
 
-    if !cookie_file.exists() {
-        println!("Fail to find cookie file: {}", cookie_file.to_string_lossy());
-        process::exit(1);
+    while !(os_params.cookie_path.is_some() && os_params.cookie_path.as_ref().unwrap().is_file() ){
+        println!("Fail to find cookie file by path: {}", &os_params.cookie_path.unwrap_or_default().to_string_lossy());
+        os_params.cookie_path = Some(PathBuf::from(read_user_input("Input path manually: ")));
     }
+    println!("Cookie file found: {}", os_params.cookie_path.as_ref().unwrap().to_string_lossy());
 
-    println!("File founded: {}", cookie_file.to_string_lossy());
+    let ket = get_cipher_key(read_user_input("enter the pass: ").as_mut_str(), os_params.iterations);
+
+    let mut cookies = extract_cookies(os_params.cookie_path.as_ref().unwrap().as_path());
+    for c in &mut cookies {
+        decrypt_cookie(c);
+//        println!("{:?}", c);
+    }
 
 }
